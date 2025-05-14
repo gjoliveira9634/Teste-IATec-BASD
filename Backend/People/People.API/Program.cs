a62567e537b6858fdddeb396c9ab45fd6405459b
G. J. Oliveira <gjoliveira.dev@outlook.com>
2025-05-14 16:48:18 -0300

    update

--- Backend/People/People.API/Program.cs
+++ Backend/People/People.API/Program.cs
@@ -90,9 +90,9 @@ using (var scope = app.Services.CreateScope())
 
 //if (app.Environment.IsDevelopment())
 //{
-    app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseDeveloperExceptionPage();
+app.UseSwagger();
+app.UseSwaggerUI();
 //}
 
 app.UseCors();
