630ca6361e596f1e5161a8330fddaa5c4ead9de0
G. J. Oliveira - Web Dev <gjoliveira.dev@outlook.com>
2025-05-13 23:44:45 -0300

    setup

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-	selector: "app-root",
-	templateUrl: "./app.component.html",
-	styleUrls: ["./app.component.scss"],
-	standalone: false, // Adicionado explicitamente
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-	title = "Banco IATEC";
+  title = 'angular-docker';
 }
